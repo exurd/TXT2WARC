51ee4cea664d43d3a921b1319dd57c573f946a0a
exurd <7785022+exurd@users.noreply.github.com>
2024-12-25 23:20:43 +0000

    uncopylocked_places: add more apis to stew

--- modules/uncopylocked_places.py
+++ modules/uncopylocked_places.py
@@ -41,6 +41,7 @@ def find_last_version(placeId):
         print("Not a vaild argument, exiting!")
         return False
 
+
 def main(place_id, data_folder):
     """
     This uses Asset Delivery API v1. This is because it uses HTTP 302
@@ -59,10 +60,17 @@ def main(place_id, data_folder):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
     print("Adding URLs to text file...")
+
+    # place apis
+    add_to_stew(f"https://www.roblox.com/games/{place_id}", file_path)
+    add_to_stew(f"https://apis.roblox.com/universes/v1/places/{place_id}/universe", file_path)
+
+    # asset id apis
+    add_to_stew(f"https://www.roblox.com/item-thumbnails?params=[{{assetId:{place_id}}}]", file_path)
     add_to_stew(f"https://www.roblox.com/places/api-get-details?assetId={place_id}", file_path)
     add_to_stew(f"https://economy.roblox.com/v2/assets/{place_id}/details", file_path)
     for i in range(amount+1):  # remember, python counts *FROM* 0!
         add_to_stew(f"https://assetdelivery.roblox.com/v1/asset/?id={place_id}&version={str(i)}", file_path)
-        # add_to_stew(f"https://assetdelivery.roblox.com/v2/assetId/{place_id}/version/{str(i)}", file_path)
+        add_to_stew(f"https://assetdelivery.roblox.com/v2/assetId/{place_id}/version/{str(i)}", file_path)
 
     return send_to_grab_site(filename, file_path, data_folder, type="uncopylocked_places")
